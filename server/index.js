d86d7b102b71092ee727000c243a609c42039e30
amritpal2 <amritpal1633.be21@chitkara.edu.in>
2023-12-30 19:23:53 +0530

    user models and middleware
    user models and middleware

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv"
+// import userRouter from "./routes/user.routes"
+import authRouter from "./routes/auth.js"
 dotenv.config()
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -11,6 +13,21 @@ mongoose.connect(process.env.MONGO).then(()=>{
 })
 
 const app=express();
+app.use(express.json())
 app.listen(3000,()=>{
     console.log("on port 3000")
 });
+
+// app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+  });
